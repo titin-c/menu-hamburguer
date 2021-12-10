8cad651634160cefba6307c408000c4d6005218f
Your NameTitín <titin.colomer.romero@gmail.com>
2021-12-10 22:56:49 +0100

    he añadido css y javscript para detectar el alto de pantalla

--- js/menu-hamburguesa.js
+++ js/menu-hamburguesa.js
@@ -1,14 +1,2 @@
-// JavaScript Document
-var items = document.getElementsByClassName('nav-link');
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener('click', printDetails);
-}
+// JavaScript Document Titín Colomer 
 
-function printDetails(e) {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].classList.contains("active")) {
-      items[i].classList.toggle("active")
-    }
-  }
-  this.classList.add("active");
-}
